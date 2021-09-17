4b4c2e2da02d8550e805a3412296e03b7be92853
UserName <Email>
2021-09-17 00:00:44 -0400

    jotting down some ideas.

--- Pantry.Data/DataBase.cs
+++ Pantry.Data/DataBase.cs
@@ -20,13 +20,21 @@ namespace Pantry.Data
         public DbSet<EquipmentCommitment> EquipmentCommitments { get; set; }
         public DbSet<Location> Locations { get; set; }
 
-        //Wow... This should all be refactored to reflect positive numbers for
-        //inputs and negative numbers for outputs.
-
-
-
-        //public DbSet<Equipment> Equipments { get; set; }
-        //public DbSet<BetterRecipeInput> BetterRecipeInputs { get; set; }
+        /*
+        Another simple recipe is just putting stuff in plastic storage containers.
+        I also need a `Containers`
+        Need a table of food instances=> Inventory.
+            Time Created, Amount, stackable, recipe used, Expiration Date
+            I could stack items if they were created at around the same time and they're stackable
+        How do I deal with items that might have a few minutes of downtime but must be checked by a human once in a while? Prioritize it? (bbq / cakes)
+        What if there were a `FoodInfo`
+            Allowable storage time at different temperatures, Nutrition
+        have `InventoryAdjustment`
+            InventoryAdjustmentReasons, 
+        InventoryAdjustmentReasons
+            Spoiled, Lost, Found, Purchased
+        `Products`
+         */
 
         public DataBase()
         {
@@ -36,8 +44,8 @@ namespace Pantry.Data
         {
             //modelBuilder.Entity<Recipe>().Ignore(x => x.RecipeSteps);
             modelBuilder.Entity<RecipeStep>()
-                .Ignore(x => x.Equipments)
-                .HasIndex(x => x.Order);
+                //.Ignore(x => x.Equipments)
+                .HasIndex(x => x.Order); //pre-optimization
             //modelBuilder.Entity<Recipe>().HasMany(x => x.RecipeFoods);
             //modelBuilder.Entity<Recipe>().HasMany(x => x.Outputs);
             //modelBuilder.Entity<Recipe>().HasKey(x => x.RecipeFoodId);
