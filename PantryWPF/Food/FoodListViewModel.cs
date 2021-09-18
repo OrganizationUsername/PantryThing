6bfef9cc30c275e48569fcb8ab10e613f1472039
UserName <Email>
2021-09-18 13:13:54 -0400

    Small change.

--- PantryWPF/Food/FoodListViewModel.cs
+++ PantryWPF/Food/FoodListViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Pantry.Data;
 using PantryWPF.Main;
 
@@ -14,13 +15,43 @@ namespace PantryWPF.Food
         private readonly IDataBase _dataBase;
         public ObservableCollection<Pantry.Core.Models.Food> Foods { get; set; }
 
+        public DelegateCommand AddRecipeCommand { get; set; }
         public FoodListViewModel()
         {
             _dataBase = new DataBase();
-            _dataBase.Foods.Add(new Pantry.Core.Models.Food() { FoodName = "Bread" });
-            Foods = new ObservableCollection<Pantry.Core.Models.Food>(_dataBase.Foods.ToList());
+            LoadFoods();
+            AddRecipeCommand = new DelegateCommand(AddFood);
         }
 
+        public void LoadFoods()
+        {
+            if (Foods is null || Foods.Count == 0)
+            {
+                Foods = new ObservableCollection<Pantry.Core.Models.Food>(_dataBase.Foods.ToList());
+                return;
+            }
+
+            for (var index = 0; index < Foods.Count; index++)
+            {
+                var x = Foods[index];
+                Foods.Remove(x);
+            }
+
+            foreach (var x in _dataBase.Foods)
+            {
+                Foods.Add(x);
+            }
+        }
+
+        public void AddFood()
+        {
+            _dataBase.Foods.Add(new Pantry.Core.Models.Food() { FoodName = "Something. " });
+            _dataBase.SaveChanges();
+        }
 
     }
+
+
+
+
 }
---
+++ PantryWPF/Main/DelegateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PantryWPF.Main
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _action;
+
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _action();
+        }
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
